3a88873462d3c67ec66599b662baf28daf7d0086
palec911 <plh911@live.com>
2023-06-22 23:49:45 +0200

    init

---
+++ src/test/java/pl/sda/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
