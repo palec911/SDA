b4ab4aac4a34baf64a35e7bb3d87922e234347c0
palec911 <plh911@live.com>
2023-06-24 23:10:25 +0200

    init

--- src/main/java/pl/sda/spring/DemoController.java
+++ src/main/java/pl/sda/spring/DemoController.java
@@ -1,6 +1,7 @@
 package pl.sda.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,14 +9,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
     private Coach myCoach;
+    private Coach anotherCoach;
 
     @Autowired
-    public DemoController(Coach theCoach) {
+    public DemoController(
+            @Qualifier("footballCoach") Coach theCoach,
+            @Qualifier("footballCoach") Coach theAnotherCoach) {
         myCoach = theCoach;
+        anotherCoach = theAnotherCoach;
     }
 
     @GetMapping("/")
     public String getDailyWorkout() {
         return myCoach.getDailyWorkout();
+    }
+
+    @GetMapping("/check")
+    public String check(){
+        return "Comparing " + (myCoach == anotherCoach);
     }
 }
