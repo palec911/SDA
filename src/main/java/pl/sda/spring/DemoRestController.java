cecffd05027204c6a5232d048544496bb7a369a3
palec911 <plh911@live.com>
2023-06-27 11:17:44 +0200

    sample rest service

---
+++ src/main/java/pl/sda/spring/DemoRestController.java
@@ -0,0 +1,15 @@
+package pl.sda.spring;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class DemoRestController {
+
+    @RequestMapping("/hello")
+    public String sayHello(){
+        return "Hello from REST Controller";
+    }
+
+}
