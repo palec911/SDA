15422635325906495e64266d672bc8d7d80a5e11
palec911 <plh911@live.com>
2023-06-28 22:24:29 +0200

    pathvariable

--- src/main/java/pl/sda/spring/controller/OpenWeatherController.java
+++ src/main/java/pl/sda/spring/controller/OpenWeatherController.java
@@ -4,10 +4,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.sda.spring.service.WeatherService;
 
 @RestController
@@ -28,8 +25,8 @@ public class OpenWeatherController {
         return ResponseEntity.ok().headers(headers).body(responseBody);
     }
 
-    @GetMapping("/weather/code/{cityCode}")
-    public ResponseEntity<String> getWeatherByCityCode(@PathVariable String cityCode) {
+    @GetMapping("/weather/code")
+    public ResponseEntity<String> getWeatherByCityCode(@RequestParam String cityCode) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         String responseBody = weatherService.getWeatherDetailsByCityCode(cityCode);
