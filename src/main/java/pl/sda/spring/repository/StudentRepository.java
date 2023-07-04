2297f11eeef20b83e228a3a4fa77fc2dd33e434d
palec911 <plh911@live.com>
2023-07-04 12:14:09 +0200

    Spring rest data

--- src/main/java/pl/sda/spring/repository/StudentRepository.java
+++ src/main/java/pl/sda/spring/repository/StudentRepository.java
@@ -1,7 +1,9 @@
 package pl.sda.spring.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import pl.sda.spring.entity.Student;
 
+@RepositoryRestResource(path = "members")
 public interface StudentRepository extends JpaRepository<Student, Long> {
 }
