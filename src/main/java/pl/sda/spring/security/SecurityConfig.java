87dd00b1fd2e2354bb6d6e7a2f79047f6f98b06d
palec911 <plh911@live.com>
2023-07-06 15:22:15 +0200

    JDBC Security

--- src/main/java/pl/sda/spring/security/SecurityConfig.java
+++ src/main/java/pl/sda/spring/security/SecurityConfig.java
@@ -5,15 +5,36 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests( config ->
+                config
+                        .requestMatchers(HttpMethod.GET, "/api/owners").hasRole("USER")
+                        .requestMatchers(HttpMethod.POST, "/api/createVet").hasRole("VET")
+                        .requestMatchers(HttpMethod.POST, "/api/createSpeciality").hasRole("ADMIN"));
+
+        http.httpBasic(Customizer.withDefaults());
+
+        http.csrf(csrf -> csrf.disable());
+        return http.build();
+    }
+
+        /*@Bean
     public InMemoryUserDetailsManager userDetailsManager() {
         UserDetails ppalczew = User.builder()
                 .username("ppalczew")
@@ -35,18 +56,5 @@ public class SecurityConfig {
 
         return new InMemoryUserDetailsManager(sda,ppalczew,ahyz);
     }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests( config ->
-                config
-                        .requestMatchers(HttpMethod.GET, "/api/owners").hasRole("USER")
-                        .requestMatchers(HttpMethod.POST, "/api/createVet").hasRole("VET")
-                        .requestMatchers(HttpMethod.POST, "/api/createSpeciality").hasRole("ADMIN"));
-
-        http.httpBasic(Customizer.withDefaults());
-
-        http.csrf(csrf -> csrf.disable());
-        return http.build();
-    }
+*/
 }
